a307891fb23867867136e6f7c76066ab2dda0619
hamay11 <yuihamada@DEKAVITAC.local>
2019-04-01 00:59:00 +0900

    [update] sytle修正

--- components/ListPage.vue
+++ components/ListPage.vue
@@ -13,6 +13,18 @@
             {{ characterObject(name).name }}
           </div>
         </div>
+        <div v-else class="characterList__item__box">
+          <div class="characterList__item__dummyImg">
+          <img
+            :src="dummy"
+            :style="{ height: 100 + '%' }"
+            border="0"
+          />
+          </div>
+          <div class="characterList__item__name">
+            ？？？
+          </div>
+        </div>
       </li>
       <Modal v-if="isModalOpen" :character="getCharacter(modalCharacter)" />
     </ul>
@@ -21,12 +33,18 @@
 <script>
 import { mapMutations, } from 'vuex';
 import { characterNames,characters, } from '@/static/config.js';
+import dummyImg from '@/static/dummy.png';
 import Modal from '@/components/Modal.vue';
 
 export default {
   components: {
     Modal,
   },
+    data () {
+        return {
+            dummy: dummyImg,
+        };
+    },
     computed: {
         captured () {
           return this.$store.state.master.characters.list;
@@ -87,6 +105,7 @@ export default {
   height: 100%;
   padding: 16px 8px;
   cursor: pointer;
+  text-align: center;
 }
 
 .characterList__item__img {
@@ -95,9 +114,19 @@ export default {
   vertical-align: middle;
 }
 
+.characterList__item__dummyImg {
+  height: 70%;
+  line-height: 16vh;
+  vertical-align: middle;
+  margin-top: 4%;
+}
+
 .characterList__item__name {
-  color: #fff;
-  width: 100%;
+  background: #fff;
+  color: #000;
+  width: 80%;
   border-bottom: 1px solid #fff;
+  border-radius: 8px;
+  margin: 0 auto;
 }
 </style>
